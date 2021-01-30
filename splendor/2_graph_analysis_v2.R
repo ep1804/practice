4c1e6771dba9abedd16fd8c5812cf6e4ea69883f
Soonmok Kwon <soonmok.kwon@navercorp.com>
2021-01-30 16:08:26 +0900

    splendor graph

--- splendor/2_graph_analysis_v2.R
+++ splendor/2_graph_analysis_v2.R
@@ -3,11 +3,12 @@ library(tidyverse)
 
 d1 = readRDS('card.rds') %>% as_tibble()
 
-g1 = make_empty_graph(5) %>%
-  set_vertex_attr('label', value = c('Ruby', 'Emerald', 'sapphire', 'Diamond', 'Onyx')) %>%
-  set_vertex_attr('color', value = c('red', 'green', 'blue', 'gray', 'black'))
 
-plot_tok_dev = function(d, merge_edge = F){
+# token - develop graph
+
+d2 = d1 %>% select(-point)
+
+plot_tok_to_dev = function(d){
   
   g1 = make_empty_graph(5) %>%
     set_vertex_attr('label', value = c('Ruby', 'Emerald', 'sapphire', 'Diamond', 'Onyx')) %>%
@@ -29,39 +30,25 @@ plot_tok_dev = function(d, merge_edge = F){
       dest = 5
     }
     
-    if (merge_edge) {
-      
-      if (row$token_ruby > 0) {
-        if (length(incident(g1, 1, c('out'))) == 0) {
-          g1 = add_edges(g1, c(1, dest), color = 'red', weight = row$token_ruby, loop.angle = 0)
-        } else {
-          eg = incident(g1, 1, c('out'))[1]
-          eg$
-            g1 = add_edges(g1, c(1, dest), color = 'red', weight = row$token_ruby, loop.angle = 0)
-        }
-      }
-      
-    } else {
-      
-      if (row$token_ruby > 0) {
-        g1 = add_edges(g1, c(1, dest), color = 'red', weight = row$token_ruby, loop.angle = 0)
-      }
-      
-      if (row$token_emerald > 0) {
-        g1 = add_edges(g1, c(2, dest), color = 'green', weight = row$token_emerald, loop.angle = pi*0.4*4)
-      }
-      
-      if (row$token_sapphire > 0) {
-        g1 = add_edges(g1, c(3, dest), color = 'blue', weight = row$token_sapphire, loop.angle = pi*0.4*3)
-      }
-      
-      if (row$token_diamond > 0) {
-        g1 = add_edges(g1, c(4, dest), color = 'gray', weight = row$token_diamond, loop.angle = pi*0.4*2)
-      }
-      
-      if (row$token_onyx > 0) {
-        g1 = add_edges(g1, c(5, dest), color = 'black', weight = row$token_onyx, loop.angle = pi*0.4*1)
-      }
+    
+    if (row$token_ruby > 0) {
+      g1 = add_edges(g1, c(1, dest), color = 'red', weight = row$token_ruby, loop.angle = 0)
+    }
+    
+    if (row$token_emerald > 0) {
+      g1 = add_edges(g1, c(2, dest), color = 'green', weight = row$token_emerald, loop.angle = pi*0.4*4)
+    }
+    
+    if (row$token_sapphire > 0) {
+      g1 = add_edges(g1, c(3, dest), color = 'blue', weight = row$token_sapphire, loop.angle = pi*0.4*3)
+    }
+    
+    if (row$token_diamond > 0) {
+      g1 = add_edges(g1, c(4, dest), color = 'gray', weight = row$token_diamond, loop.angle = pi*0.4*2)
+    }
+    
+    if (row$token_onyx > 0) {
+      g1 = add_edges(g1, c(5, dest), color = 'black', weight = row$token_onyx, loop.angle = pi*0.4*1)
     }
   }
   
@@ -78,29 +65,67 @@ plot_tok_dev = function(d, merge_edge = F){
        edge.width = E(g1)$weight * 1.7 + 1)
 }
 
-d1 %>% plot_tok_dev
+d1 %>% plot_tok_to_dev
 
-d1 %>% filter(develop == 'diamond') %>% plot_tok_dev
-d1 %>% filter(develop == 'diamond' & level == 1) %>% plot_tok_dev
-d1 %>% filter(develop == 'diamond' & level == 2) %>% plot_tok_dev
-d1 %>% filter(develop == 'diamond' & level == 3) %>% plot_tok_dev
+d1 %>% filter(develop == 'diamond') %>% plot_tok_to_dev
+d1 %>% filter(develop == 'diamond' & level == 1) %>% plot_tok_to_dev
+d1 %>% filter(develop == 'diamond' & level == 2) %>% plot_tok_to_dev
+d1 %>% filter(develop == 'diamond' & level == 3) %>% plot_tok_to_dev
 
-d1 %>% filter(develop == 'sapphire') %>% plot_tok_dev
-d1 %>% filter(develop == 'sapphire' & level == 1) %>% plot_tok_dev
-d1 %>% filter(develop == 'sapphire' & level == 2) %>% plot_tok_dev
-d1 %>% filter(develop == 'sapphire' & level == 3) %>% plot_tok_dev
+d1 %>% filter(develop == 'sapphire') %>% plot_tok_to_dev
+d1 %>% filter(develop == 'sapphire' & level == 1) %>% plot_tok_to_dev
+d1 %>% filter(develop == 'sapphire' & level == 2) %>% plot_tok_to_dev
+d1 %>% filter(develop == 'sapphire' & level == 3) %>% plot_tok_to_dev
 
-d1 %>% filter(develop == 'emerald') %>% plot_tok_dev
-d1 %>% filter(develop == 'emerald' & level == 1) %>% plot_tok_dev
-d1 %>% filter(develop == 'emerald' & level == 2) %>% plot_tok_dev
-d1 %>% filter(develop == 'emerald' & level == 3) %>% plot_tok_dev
+d1 %>% filter(develop == 'emerald') %>% plot_tok_to_dev
+d1 %>% filter(develop == 'emerald' & level == 1) %>% plot_tok_to_dev
+d1 %>% filter(develop == 'emerald' & level == 2) %>% plot_tok_to_dev
+d1 %>% filter(develop == 'emerald' & level == 3) %>% plot_tok_to_dev
 
-d1 %>% filter(develop == 'ruby') %>% plot_tok_dev
-d1 %>% filter(develop == 'ruby' & level == 1) %>% plot_tok_dev
-d1 %>% filter(develop == 'ruby' & level == 2) %>% plot_tok_dev
-d1 %>% filter(develop == 'ruby' & level == 3) %>% plot_tok_dev
+d1 %>% filter(develop == 'ruby') %>% plot_tok_to_dev
+d1 %>% filter(develop == 'ruby' & level == 1) %>% plot_tok_to_dev
+d1 %>% filter(develop == 'ruby' & level == 2) %>% plot_tok_to_dev
+d1 %>% filter(develop == 'ruby' & level == 3) %>% plot_tok_to_dev
 
-d1 %>% filter(develop == 'onyx') %>% plot_tok_dev
-d1 %>% filter(develop == 'onyx' & level == 1) %>% plot_tok_dev
-d1 %>% filter(develop == 'onyx' & level == 2) %>% plot_tok_dev
-d1 %>% filter(develop == 'onyx' & level == 3) %>% plot_tok_dev
+d1 %>% filter(develop == 'onyx') %>% plot_tok_to_dev
+d1 %>% filter(develop == 'onyx' & level == 1) %>% plot_tok_to_dev
+d1 %>% filter(develop == 'onyx' & level == 2) %>% plot_tok_to_dev
+d1 %>% filter(develop == 'onyx' & level == 3) %>% plot_tok_to_dev
+
+# lets focus on color relation. do merge edges
+
+
+d3 = d2 %>%
+  group_by(level, develop) %>%
+  summarise(token_diamond = sum(token_diamond),
+            token_sapphire = sum(token_sapphire),
+            token_emerald = sum(token_emerald),
+            token_ruby = sum(token_ruby),
+            token_onyx = sum(token_onyx))
+
+print(d3)
+
+d3 %>% filter(develop == 'diamond') %>% plot_tok_to_dev
+d3 %>% filter(develop == 'diamond' & level == 1) %>% plot_tok_to_dev
+d3 %>% filter(develop == 'diamond' & level == 2) %>% plot_tok_to_dev
+d3 %>% filter(develop == 'diamond' & level == 3) %>% plot_tok_to_dev
+
+d3 %>% filter(develop == 'sapphire') %>% plot_tok_to_dev
+d3 %>% filter(develop == 'sapphire' & level == 1) %>% plot_tok_to_dev
+d3 %>% filter(develop == 'sapphire' & level == 2) %>% plot_tok_to_dev
+d3 %>% filter(develop == 'sapphire' & level == 3) %>% plot_tok_to_dev
+
+d3 %>% filter(develop == 'emerald') %>% plot_tok_to_dev
+d3 %>% filter(develop == 'emerald' & level == 1) %>% plot_tok_to_dev
+d3 %>% filter(develop == 'emerald' & level == 2) %>% plot_tok_to_dev
+d3 %>% filter(develop == 'emerald' & level == 3) %>% plot_tok_to_dev
+
+d3 %>% filter(develop == 'ruby') %>% plot_tok_to_dev
+d3 %>% filter(develop == 'ruby' & level == 1) %>% plot_tok_to_dev
+d3 %>% filter(develop == 'ruby' & level == 2) %>% plot_tok_to_dev
+d3 %>% filter(develop == 'ruby' & level == 3) %>% plot_tok_to_dev
+
+d3 %>% filter(develop == 'onyx') %>% plot_tok_to_dev
+d3 %>% filter(develop == 'onyx' & level == 1) %>% plot_tok_to_dev
+d3 %>% filter(develop == 'onyx' & level == 2) %>% plot_tok_to_dev
+d3 %>% filter(develop == 'onyx' & level == 3) %>% plot_tok_to_dev
